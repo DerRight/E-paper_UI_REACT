ecedc5444d85ed0d743c832b4eeec3f862974e18
DerRight <zxc123456789123456@gmail.com>
2024-07-08 17:54:37 +0800

    新增了Sidebar然後設置好其他頁面

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,38 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import LOGO from '../image/EpaperLogo_BLACK.png';
+import './Sidebar.scss';
 
 function Sidebar() {
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className="Sidebar">
+            <div className="sidebar-header">
+                <img src={LOGO} alt="logo" />
+            </div>
+
+            <ul className="sidebar-menu">
+                <li>
+                    <Link to="/home">首頁</Link>
+                </li>
+
+                <li>
+                    <Link to="/search">搜尋</Link>
+                </li>
+
+                <li>
+                    <Link to="/favorite">我的最愛</Link>
+                </li>
+
+                <li>
+                    <Link to="/Setting">設定</Link>
+                </li>
+
+                <li>
+                    <Link to="/Chart">圖表</Link>
+                </li>
+            </ul>
+
+            <div className="sidebar-footer"></div>
         </div>
     );
 }
